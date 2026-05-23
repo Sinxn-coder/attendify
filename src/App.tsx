7f8b1c48fb8ed732b9ead4d6ef66c6c2037f21db
Sinxn-coder <msinankavala786@gmail.com>
2026-05-23 21:50:29 +0530

    Fix React Router basename for GitHub Pages blank screen issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import StudentDashboard from './pages/StudentDashboard';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
